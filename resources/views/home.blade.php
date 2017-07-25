1144f12772d2e2d4c573faf3be626401c3ad61ae
Alexandr Kuznetsov <my@mail.ru>
2017-07-26 02:21:15 +0300

    Изменен вид главной страницы и страницы регистрации

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,28 @@
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../public/css/index.css">
 </head>
 <body>
-<a href="register">Sign Up</a>
-<a href="login">Log In</a>
-
-@if(session()->has('login'))
-    <a href="profile">Profile</a>
-
-    @if (session('role')==3)
-        <a href="allprofiles">All Profiles</a>
-    @endif
-    @if(session('role')!=2) <!--роль 2 это забаненый пользователь-->
-        <a href="addpost">Post your recipe</a>
+<div class="headerWrapper">
+<header id="siteHeader">
+    <div class="menuItems">
+        @if(!session()->has('login'))
+        <a href="register"><span class="menuItems-item">Sign Up</span></a>
+        <a href="login"><span class="menuItems-item">Log In</span></a>
         @endif
-<a href="../public/exit">Exit</a>
-    @endif
+        @if(session()->has('login'))
+            <a href="profile"><span class="menuItems-item">Profile</span></a>
+
+            @if (session('role')==3)
+                <a href="allprofiles"><span class="menuItems-item">All Profiles</span></a>
+            @endif
+            @if(session('role')!=2) <!--роль 2 это забаненый пользователь-->
+                <a href="addpost"><span class="menuItems-item">Post your recipe</span></a>
+                @endif
+        <a href="../public/exit"><span class="menuItems-item">Exit</span></a>
+            @endif
+    </div>
+</header>
+</div>
 </body>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,36 +3,56 @@
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../public/css/index.css">
+    <style>
+        body{
+            background-image: url(../public/css/images/registration_bg.jpg);
+        }
+    </style>
 </head>
 <body>
+<div class="headerWrapper">
+    <header id="siteHeader">
+        <div class="menuItems">
+            <a href="login"><span class="menuItems-item">Log In</span></a>
+            <a href="home"><span class="menuItems-item">Home</span></a>
+        </div>
+    </header>
+</div>
+
         @if(count($errors)>0)
         @foreach($errors->all() as $error)
             <li>{{$error}}</li>
             @endforeach
         @endif
-<?php echo' <form action="registration"  enctype="multipart/form-data" method="POST">
+        <div class="bodywarp">
+            <div class="bodywarp__registration">
+                <div class="bodywarp__registration-label">
+                    <span class="bodywarp__registration-text">Sing Up</span>
+                </div>
+<?php echo' <form action="registration"  enctype="multipart/form-data" method="POST" class="bodywarp__registration-form">
     ' . csrf_field() . '
     <label>Login:</label>
-    <input type="text" name="login"><br>
+    <input type="text" name="login" class ="registration-input"><br>
     <label>Password:</label>
-    <input type="password" name="pass"><br>
+    <input type="password" name="pass" class ="registration-input"><br>
     <label>Email:</label>
-    <input type="text" name="email"><br>
+    <input type="text" name="email" class ="registration-input"><br>
     <label>First name:</label>
-    <input type="text" name="first_name"><br>
+    <input type="text" name="first_name" class ="registration-input"><br>
     <label>Surname:</label>
-    <input type="text" name="surname"><br>
+    <input type="text" name="surname" class ="registration-input"><br>
     <label>Your gender:</label>
-    <select name="gender">
+    <select name="gender" class ="registration-input">
         <option>Male</option>
         <option>Female</option>
     </select><br>
     <label>Mobile number:</label>
-    <input type="tel" pattern="[0-9]{11}" name="mobile"><br>
-    <label>Upload your avatar</label>
-    <input type="file" name="avatar"><br>
-    <input type="submit" value="Create an account" />
+    <input type="tel" pattern="[0-9]{11}" name="mobile" class ="registration-input"><br>
+    <label id="input__label-avatar">Upload your avatar</label>
+    <input type="file" name="avatar" class ="registration-input"><br>
+    <input type="submit" value="Create an account" class ="input__button" />
 </form>';?>
-        <a href="login">Log In</a>
-        <a href="home">Home</a>
+                </div>
+            </div>
 </body>
